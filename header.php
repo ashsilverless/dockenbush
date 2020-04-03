465682b26d5dbcbfbf4b922e674c169c4be5640f
Ash - SIlverless <ash@silverless.co.uk>
2020-04-03 15:55:53 +0100

    DEV - add favicon and header data

--- header.php
+++ header.php
@@ -10,17 +10,17 @@
 <html <?php language_attributes(); ?>>
 <head>
  	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">   
-    <meta name="description" content="Luxury Lodge, South Central Rift, Kenya">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Dockenbush Holiday Cottage, near Harrogate, nestles in the heart of North Yorkshire’s countryside surrounded by rolling fields and woods tucked away privately from North Dockenbush, the main house.">
     <meta name="keywords" content=" ">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	
+
 	<title>Dockenbush Cottage</title>
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Muli:300,400,700&display=swap" rel="stylesheet">
-	
+
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -28,7 +28,7 @@
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
-	
+
 	<?php wp_head(); ?>
 </head>
 
